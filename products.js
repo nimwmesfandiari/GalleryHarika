f55fbb4e05acf351d5b99ba2f7f06fa27caec06d
nimwmesfandiari <nimwmesfandiarii@gmail.com>
2026-08-09 12:29:10 +0330

    Create products.js

---
+++ products.js
@@ -0,0 +1,39 @@
+// دیتابیس محصولات فروشگاه هاریکا
+const productsDB = [
+    {
+        id: 1,
+        category: "تیشرت",
+        subStyle: "تیشرت باکسی",
+        size: "XL",
+        title: "تیشرت باکسی هاریکا Oval",
+        price: "680,000",
+        fabric: "۱۰۰٪ نخ پنبه سوپر شانه شده (دورس دو نخ بدون کرک)",
+        colors: ["#000000", "#ffffff", "#808080"], // مشکی، سفید، طوسی
+        image: "https://s17.uupload.ir/files/logopng/logo.png",
+        stock: 5
+    },
+    {
+        id: 2,
+        category: "تیشرت",
+        subStyle: "تیشرت باکسی",
+        size: "XL",
+        title: "تیشرت باکسی Minimal",
+        price: "720,000",
+        fabric: "پنبه لاکرا درجه یک با کشسانی بالا",
+        colors: ["#5c4033", "#000000"], // قهوه‌ای، مشکی
+        image: "https://s17.uupload.ir/files/logopng/logo.png",
+        stock: 2
+    },
+    {
+        id: 3,
+        category: "شلوار لی",
+        subStyle: "شلوار مام فیت",
+        size: "32",
+        title: "شلوار لی مام فیت زغال‌سنگی",
+        price: "1,100,000",
+        fabric: "جین ۱۲ اونس سنگ‌شور شده بدون آبرفت",
+        colors: ["#333333", "#1a252f"],
+        image: "https://s17.uupload.ir/files/logopng/logo.png",
+        stock: 1
+    }
+];
